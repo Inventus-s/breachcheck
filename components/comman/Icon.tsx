a878b908fec7d924f6c54f12af6376337e570e95
Mukund Kumar <123208378+Inventus-s@users.noreply.github.com>
2026-04-13 23:59:49 +0530

    correction

--- components/comman/Icon.tsx
+++ components/comman/Icon.tsx
@@ -37,7 +37,7 @@ const icons = {
         </svg>
     ),
 
-    cloudlock: (
+    cloudLock: (
         <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-cloud-lock">
             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
             <path d="M19 18a3.5 3.5 0 0 0 0 -7h-1c.397 -1.768 -.285 -3.593 -1.788 -4.787c-1.503 -1.193 -3.6 -1.575 -5.5 -1s-3.315 2.019 -3.712 3.787c-2.199 -.088 -4.155 1.326 -4.666 3.373c-.512 2.047 .564 4.154 2.566 5.027" />
